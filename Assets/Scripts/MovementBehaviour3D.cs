658998b413fb0aa1f93c6db082156840c437688a
MrGri00 <marcgarciat2000@gmail.com>
2022-02-09 14:22:46 +0100

    Version 1.1.1
    · Created materials for player and obstacles
    · Added an obstacle cube in scene for testing purposes

--- Assets/Scripts/MovementBehaviour3D.cs
+++ Assets/Scripts/MovementBehaviour3D.cs
@@ -8,14 +8,14 @@ public class MovementBehaviour3D : MonoBehaviour
     [SerializeField] private float speed = 10f;
 
     // CALCULATIONS
-    Vector3 vec3 = new Vector3(0, 0, 0);
+    Vector3 vect3 = new Vector3(0, 0, 0);
 
     public void Move(float input)
     {
-        vec3.x = input;
-        vec3.y = 0;
-        vec3.z = 1;
+        vect3.x = input;
+        vect3.y = 0;
+        vect3.z = 1;
 
-        transform.Translate(vec3 * speed * Time.deltaTime);
+        transform.Translate(vect3 * speed * Time.deltaTime);
     }
 }
